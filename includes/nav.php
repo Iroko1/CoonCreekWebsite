2ba71b40e062eb9cb30ffed68a9399b2e44dd1e5
Melissa-Mika <melissamika3@gmail.com>
2024-03-20 10:43:01 -0500

    initial commit

---
+++ includes/nav.php
@@ -0,0 +1,35 @@
+
+</php include "includes/cdnlinks.php" ?>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/cooncreekbaptistchurch/index.php"><img src="/cooncreekbaptistchurch/view/images/CoonCreekLogo.jpeg" alt="Coon Creek Baptist Church Logo" width="30" height="24"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/cooncreekbaptistchurch/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/cooncreekbaptistchurch/view/signup_form.php">Sign Up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/cooncreekbaptistchurch/view/prayer_request.php">Prayer Request</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Ministries
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/ministries.php">TeamKID</a></li>
+            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/ministries.php">Youth Group</a></li>
+            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/ministries.php">TLC Women's Group</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coon Creek Baptist Church</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ruthie&display=swap" rel="stylesheet">
+
+<link rel= "stylesheet" href= "./view/styles.css">
+
+</head>
+<body>
+
+<?php include "includes/cdnlinks.php" ?>
+<?php include "includes/nav.php" ?>
+
+<h1>Coon Creek Baptist Church</h1>
+<h2>Gather at the Creek</h2>
+
+<img src = "view/images/CoonCreekLogo.jpeg" alt = "Coon Creek Baptist Church Logo">
+
+
+
+
+
+    
+</body>
+</html>
