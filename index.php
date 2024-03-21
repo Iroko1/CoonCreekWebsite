9466fafd716300bffcf3ab1d597c4f0432808654
Iroko1 <96309975+Iroko1@users.noreply.github.com>
2024-03-21 15:22:39 -0500

    added calendar - needs better styling

--- index.php
+++ index.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coon Creek Baptist Church</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Ruthie&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ruthie&display=swap" rel="stylesheet">
 
-<link rel= "stylesheet" href= "./view/styles.css">
+    <link rel="stylesheet" href="./view/styles.css">
 
 </head>
+
 <body>
 
-<?php include "includes/cdnlinks.php" ?>
-<?php include "includes/nav.php" ?>
+    <?php include "includes/cdnlinks.php" ?>
+    <?php include "includes/nav.php" ?>
+    <header>
+        <h1>Coon Creek Baptist Church</h1>
+        <h2>Gather at the Creek</h2>
+        <img src="view/images/CoonCreekLogo.jpeg" alt="Coon Creek Baptist Church Logo">
+    </header>
 
-<h1>Coon Creek Baptist Church</h1>
-<h2>Gather at the Creek</h2>
+    <!-- Either option is fine -->
+    <div>
+        <iframe src="https://calendar.google.com/calendar/embed?src=cooncreekbaptist%40gmail.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
 
-<img src = "view/images/CoonCreekLogo.jpeg" alt = "Coon Creek Baptist Church Logo">
+    <div>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23ffffff&src=Y29vbmNyZWVrYmFwdGlzdEBnbWFpbC5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
 
 
 
 
 
-    
+
+
+
 </body>
+
 </html>
