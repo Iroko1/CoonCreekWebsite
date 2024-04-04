0e98717ee9c38a50a72d508f4fab72521e0764cb
Alyssa Cooper <alyssajcooper3@gmail.com>
2024-04-04 14:22:35 -0500

    Alyssa's edits

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,64 @@
 
 </php include "includes/cdnlinks.php" ?>
 
-
+<!-- navigation  bar -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php">IMAGE</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+
+              <!-- <li class="nav-item">
+                <a class="nav-link" href="about.php">idk</a>
+              </li> -->
+
+              <!-- about tab includes History and Pastors -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  About
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="history.php">History</a></li>
+                  <li><a class="dropdown-item" href="pastors.php">Pastors</a></li>
+                </ul>
+              </li>
+
+              <!-- Ministries tab includes Team Kids, Youth Group, and TLC Women's Group -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Ministries
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="teamkids.php">TEAM Kids</a></li>
+                  <li><a class="dropdown-item" href="youth.php">Youth Group</a></li>
+                  <li><a class="dropdown-item" href="womens.php">TLC Women's Group</a></li>
+                </ul>
+              </li>
+
+              <!-- get involved tab includes Sign Up and Request Info -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Get Involved
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="signup.php">Sign-up</a></li>
+                  <li><a class="dropdown-item" href="requestinfo.php">Request Info</a></li>
+                </ul>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="/cooncreekbaptistchurch/index.php"><img src="/cooncreekbaptistchurch/view/images/CoonCreekLogo.jpeg" alt="Coon Creek Baptist Church Logo" width="30" height="24"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,4 +88,4 @@
       </ul>
     </div>
   </div>
-</nav>
+</nav> -->
--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ruthie&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="./view/styles.css">
 
 </head>
@@ -24,22 +23,29 @@
         <img src="view/images/CoonCreekLogo.jpeg" alt="Coon Creek Baptist Church Logo">
     </header>
 
+    <!-- Cannot have youtube video and calendar on same page??? -AC -->
+    <div class="container-md">
+        <!-- <iframe 
+            src="http://www.youtube.com/embed?listType=playlist&list=UURG9J9XbYfDmI1BB6U0puzA">
+        </iframe> -->
+    </div>
+
     <!-- Either option is fine -->
-    <div>
+    <!-- <div>
         <iframe src="https://calendar.google.com/calendar/embed?src=cooncreekbaptist%40gmail.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div> -->
+
+    <div class="container-md">
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23ffffff&src=Y29vbmNyZWVrYmFwdGlzdEBnbWFpbC5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000&color=%230B8043" style="border:solid 3px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
 
-    <div>
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23ffffff&src=Y29vbmNyZWVrYmFwdGlzdEBnbWFpbC5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-    </div>
 
 
 
 
 
-
-
-
+    <!-- I don't know if we need this or not -AC -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>        
 </body>
 
 </html>
