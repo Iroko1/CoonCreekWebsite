77358929cfcb3dd033defc45f52f134f8c1ef614
Alyssa Cooper <alyssajcooper3@gmail.com>
2024-04-12 13:55:01 -0500

    added cards to about.php

--- view/about.php
+++ view/about.php
@@ -6,6 +6,21 @@
     <title>About Us</title>
 </head>
 <body>
-    
+    <div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+    <img src="..." class="card-img-bottom" alt="...">
+    </div>
+    <div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+    <img src="..." class="card-img-bottom" alt="...">
+    </div>
 </body>
 </html>
