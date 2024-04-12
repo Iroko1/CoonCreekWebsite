a5bd7c93c79bf275ed82d63a471f18562aee47a1
Melissa-Mika <melissamika3@gmail.com>
2024-04-12 13:58:56 -0500

    changed styling

--- includes/nav.php
+++ includes/nav.php
@@ -3,14 +3,14 @@
 <!-- navigation  bar -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/cooncreekbaptistchurch/index.php"><img src="/cooncreekbaptistchurch/view/images/light-maroon.png" alt="Coon Creek Baptist Church Logo" width="60" height="54"></a>
+    <a class="navbar-brand" href="/index.php"><img src="/view/images/light-maroon.png" alt="Coon Creek Baptist Church Logo" width="60" height="54"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/cooncreekbaptistchurch/index.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="/index.php">Home</a>
         </li>
 
         <!-- About dropdown includes 'Pastors' and 'History' -->
@@ -19,8 +19,8 @@
             About
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/history.php">History</a></li>
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/pastors.php">Pastors</a></li>
+            <li><a class="dropdown-item" href="/view/history.php">History</a></li>
+            <li><a class="dropdown-item" href="/view/pastors.php">Pastors</a></li>
           </ul>
         </li>
 
@@ -30,8 +30,8 @@
             Get Involved
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/signup_form.php">Request Info</a></li>
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/prayer_request.php">Prayer Request</a></li>
+            <li><a class="dropdown-item" href="/view/signup_form.php">Request Info</a></li>
+            <li><a class="dropdown-item" href="/view/prayer_request.php">Prayer Request</a></li>
           </ul>
         </li>
 
@@ -40,9 +40,9 @@
             Ministries
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/ministries.php">TeamKID</a></li>
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/ministries.php">Youth Group</a></li>
-            <li><a class="dropdown-item" href="/cooncreekbaptistchurch/view/ministries.php">TLC Women's Group</a></li>
+            <li><a class="dropdown-item" href="/view/ministries.php">TeamKID</a></li>
+            <li><a class="dropdown-item" href="/view/ministries.php">Youth Group</a></li>
+            <li><a class="dropdown-item" href="/view/ministries.php">TLC Women's Group</a></li>
           </ul>
         </li>
       </ul>
