dc99f35c01a282d333886def568462791060658f
Alyssa Cooper <alyssajcooper3@gmail.com>
2024-04-23 15:11:23 -0500

    changes to about.php

--- view/about.php
+++ view/about.php
@@ -5,19 +5,46 @@
 
 <h3 class="about-title">About</h3>
 
-<div class="card">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="view/images/cross.jpg" class="img-fluid rounded-start" alt="...">
     </div>
-    <img src="..." class="card-img-bottom" alt="...">
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">History</h5>
+        <p class="card-text">Coon Creek Baptist Church was established in 1840. The creation of our church was pioneered by D. R. Murphy, Reverend L. R. Ashworth, the Culbertsons, and the Lawlers. In its history, Coon Creek has been referred to as the "lighthouse on the hill".</p>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="card">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="view/images/Durnell.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Pastor: Zach Durnell</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
     </div>
-    <img src="..." class="card-img-bottom" alt="...">
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="view/images/McCarter.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Youth Pastor: Justen McCarter</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
 </div>
