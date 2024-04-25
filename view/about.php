0bafd698b262c80b545e3bc2b4deed29c9608127
Melissa-Mika <melissamika3@gmail.com>
2024-04-25 10:53:32 -0500

    modified about page

--- view/about.php
+++ view/about.php
@@ -3,10 +3,45 @@
 
 <link rel="stylesheet" href="../view/styles.css">
 
-<h3 class="about-title">About</h3>
+<h3 class="about-title">About Us</h3>
 
 <body>
-<div class="card mb-3" style="max-width: 540px;">
+
+<div class="content-wrap">
+
+<section>
+    <img src="../view/images/cross.jpg" alt="..." height="500" width="450" class="left">
+    <div class="history-text-group">
+    <h2 class="historyheading">History of Our Church</h2>
+    <p class="historytext">Coon Creek Baptist Church was established in 1840. The creation of our church was pioneered by D. R. Murphy, Reverend L. R. Ashworth, the Culbertsons, and the Lawlers. In its history, Coon Creek has been referred to as the "lighthouse on the hill".</p>
+    </div>
+    <p class="clearFloat">&nbsp;</p>
+</section>
+
+<section class="clearFloat">
+    
+    <h2 class="pastor1">Pastor: Zach Durnell</h2>
+    <p class="clearFloat">&nbsp;</p>
+    <img src="../view/images/Durnell.png" alt="..." height="500" width="450" class="right">
+</section>
+
+<section>
+  <img src="../view/images/McCarter.png" alt="..." height="450" width="525" class="left2">
+  <h2 class="pastor2">Youth Pastor: Justen McCarter</h2>
+  <p class="clearFloat">&nbsp;</p>
+</section>
+
+
+
+
+
+</div>
+
+
+
+
+
+<!-- <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
       <img src="../view/images/cross.jpg" class="img-fluid rounded-start" alt="...">
@@ -46,5 +81,5 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 </body>
