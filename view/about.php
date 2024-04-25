65a5fda46b9a4ba03d339f3b29a7a2501f73265a
Melissa-Mika <melissamika3@gmail.com>
2024-04-25 12:11:13 -0500

    updated about page

--- view/about.php
+++ view/about.php
@@ -6,43 +6,54 @@
 <h3 class="about-title">About Us</h3>
 
 <body>
-<<<<<<< HEAD
 
-<div class="content-wrap">
+  <div class="content-wrap">
 
-<section>
-    <img src="../view/images/cross.jpg" alt="..." height="500" width="450" class="left">
-    <div class="history-text-group">
-    <h2 class="historyheading">History of Our Church</h2>
-    <p class="historytext">Coon Creek Baptist Church was established in 1840. The creation of our church was pioneered by D. R. Murphy, Reverend L. R. Ashworth, the Culbertsons, and the Lawlers. In its history, Coon Creek has been referred to as the "lighthouse on the hill".</p>
-    </div>
-    <p class="clearFloat">&nbsp;</p>
-</section>
+    <section>
+      <div class="image-container">
+        <img src="../view/images/cross.jpg" alt="..." height="500" width="450" class="left">
+      </div>
+      <div class="history-text-group">
+        <h2 class="historyheading">History of Our Church</h2>
+        <p class="historytext">Coon Creek Baptist Church was established in 1840. The creation of our church was pioneered by D. R. Murphy, Reverend L. R. Ashworth, the Culbertsons, and the Lawlers. In its history, Coon Creek has been referred to as the "lighthouse on the hill".</p>
+      </div>
+      <p class="clearFloat">&nbsp;</p>
+    </section>
 
-<section class="clearFloat">
-    
-    <h2 class="pastor1">Pastor: Zach Durnell</h2>
-    <p class="clearFloat">&nbsp;</p>
-    <img src="../view/images/Durnell.png" alt="..." height="500" width="450" class="right">
-</section>
+    <section class="clearFloat">
+      <div class="pastor-container">
+        <h2 class="pastor1">Pastor: Zach Durnell</h2>
+        <h3 class="pastor-info">Pastor since 2014</h3>
+      </div>
 
-<section>
-  <img src="../view/images/McCarter.png" alt="..." height="450" width="525" class="left2">
-  <h2 class="pastor2">Youth Pastor: Justen McCarter</h2>
-  <p class="clearFloat">&nbsp;</p>
-</section>
+      <div class="image-container">
+        <img src="../view/images/Durnell.png" alt="..." height="500" width="450" class="right">
+      </div>
+      <p class="clearFloat">&nbsp;</p>
+    </section>
+
+    <section>
+      <div class="image-container">
+        <img src="../view/images/McCarter.png" alt="..." height="450" width="525" class="left2">
+      </div>
+      <div class="pastor2-container">
+        <h2 class="pastor2">Justen McCarter</h2>
+        <h3 class="pastor2-info">Associate Pastor/Youth Pastor</h3>
+      </div>
+      <p class="clearFloat">&nbsp;</p>
+    </section>
 
 
 
 
 
-</div>
+  </div>
 
 
 
 
 
-<!-- <div class="card mb-3" style="max-width: 540px;">
+  <!-- <div class="card mb-3" style="max-width: 540px;">
 =======
 <div class="card mb-3">
 >>>>>>> 781a6c607230ce94022778c347a692a9bcf75f02
